c2b75d1e6426a76dd96ab269108e237724a71e85
brakhais <isaacbrakha14@gmail.com>
2023-10-30 14:06:50 -0500

    wrote code for secret code generation

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-console.log("connected")
+// -----DOM ELEMENTS----//
 
-const div = document.querySelector('#first')
-div.style.borderColor = 'red'
+const coloredPegs = ['brown', 'gold', 'purple', 'green', 'red', 'blue']
+
+const secretCodeEl = document.querySelectorAll('#solution')
+
+
+
+function secretCode() {
+    let code = []
+    for (let i = 0; i < 4; i++) {
+        const rndmInt = Math.floor(Math.random() * coloredPegs.length)
+        code.push(coloredPegs[rndmInt])
+        console.log(code)
+    }
+}
+
+
+secretCode()
+
+
+// console.log(code)
+// let peg = document.createElement('div')
+// peg.style.height = '80%'
+// peg.style.width = '80%'
+// peg.style.borderRadius = '50%'
+// peg.style.backgroundColor = coloredPegs[rndmInt]
+// secretCodeEl[i].appendChild(peg)
+
